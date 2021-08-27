bf674c87b8f8206692c6e86c1fa35cfe968871de
VictorDamian <vdamianbarron@gmail.com>
2021-08-27 18:14:24 -0500

    POO

---
+++ Persona.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace POO_C_
+{
+    public class Persona
+    {
+        public string nombre;
+        public int edad;
+
+        public void Hablar()
+        {
+            Console.WriteLine("Me llamo " + nombre +" y tengo "+edad+" años");
+        }
+        public void Caminar(){
+
+        }
+        public void Comer(){
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace POO_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona people = new Persona();
+            people.nombre = "Victor";
+            people.edad = 24;
+            people.Hablar();
+        }
+    }
+}
