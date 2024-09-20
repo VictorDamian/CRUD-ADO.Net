d2764ef267a558de217285870cafbd78d2262d5f
VictorDamian <vdamianbarron@gmail.com>
2024-09-19 18:03:44 -0600

    descripcion factory

--- DesignPatter/Patterns/Program.cs
+++ DesignPatter/Patterns/Program.cs
@@ -19,6 +19,44 @@ Console.WriteLine("--------------");
 
 #region Factory simple
 
+// Los patrones Factory Simple y Factory Abstract son patrones de diseño creacionales que facilitan
+// la creación de objetos sin especificar la clase exacta de objeto que se va a crear.
+
+// Factory Simple
+// Concepto: El patrón Factory Simple se utiliza para encapsular la creación de objetos en una
+// única clase llamada "fábrica". Esta clase decide qué objeto crear en función de algunos parámetros.
+
+// Funcionamiento:
+
+// Interfaz o Clase Base: Define una interfaz común para todos los productos que se van a crear.
+// Fábrica: Tiene un método que recibe parámetros (como el tipo de objeto a crear) y retorna una instancia del producto correspondiente.
+// Implementaciones: Las clases concretas implementan la interfaz y proporcionan la funcionalidad específica.
+
+var facDb = new Factory().GetFactory(ConnectionService.DB);
+var oConn = facDb.GetDB(ConnectionProviderType.MySQLConnection);
+//Console.WriteLine(oConn.Connections());
+
+var facApi = new Factory().GetFactory(ConnectionService.Rest);
+var oConnApi = facApi.GetRest(ConnectionProviderType.SalesRest);
+//Console.WriteLine(oConnApi.readURL("localhost"));
+
+#endregion
+
+#region Factory Abstract
+
+// Concepto: El patrón Factory Abstract es una versión más avanzada del patrón Factory Simple.
+// Permite crear familias de productos relacionados sin especificar sus clases concretas.
+// Se utiliza cuando el sistema debe ser independiente de cómo se crean, componen y representan los productos.
+
+// Funcionamiento:
+
+// Interfaz de Fábrica Abstracta: Define métodos para crear cada tipo de producto
+// (por ejemplo, crear un tipo de conexión a base de datos o un tipo de conexión REST).
+
+// Clases de Fábrica Concretas: Implementan la interfaz de fábrica abstracta y crean instancias de productos específicos.
+// Interfaz o Clases Base para Productos: Similar al Factory Simple, pero puede haber
+// múltiples interfaces o clases base para diferentes familias de productos.
+
 Factory factory = new Factory();
 
 // Usar la fábrica para obtener la conexión a la base de datos
@@ -34,44 +72,35 @@ string data = restConnection.readURL("https://api.example.com/data");
 
 #endregion
 
-#region Factory Abstract
-var facDb = new Factory().GetFactory(ConnectionService.DB);
-var oConn = facDb.GetDB(ConnectionProviderType.MySQLConnection);
-Console.WriteLine(oConn.Connections());
-
-var facApi = new Factory().GetFactory(ConnectionService.Rest);
-var oConnApi = facApi.GetRest(ConnectionProviderType.SalesRest);
-Console.WriteLine(oConnApi.readURL("localhost"));
-#endregion
-
 #region Observable
 
-//OrderService _orderService = new OrderService();
+OrderService _orderService = new OrderService();
 
-//var order =  new Order(){
-//        OrderNumber = "89675RCSG",
-//        OrderDate = DateTime.Now,
-//        TotalAmount = 105.99m,
-//        OrderStatus = OrderStatuses.PendingPayment
-//    };
-////
-//Console.WriteLine("\nAttaching observer...");
+var order = new Order()
+{
+    OrderNumber = "89675RCSG",
+    OrderDate = DateTime.Now,
+    TotalAmount = 105.99m,
+    OrderStatus = OrderStatuses.PendingPayment
+};
+//
+Console.WriteLine("\nAttaching observer...");
 
-//var smsObs = new SMSObserver();
-//var emailObs = new EmailObserver();
+var smsObs = new SMSObserver();
+var emailObs = new EmailObserver();
 
-//_orderService.Attach(smsObs);
-//_orderService.Attach(emailObs);
+_orderService.Attach(smsObs);
+_orderService.Attach(emailObs);
 
-//Console.WriteLine("Updating ORder Status....");
+Console.WriteLine("Updating ORder Status....");
 
-//_orderService.UpdateOrder(order);
+_orderService.UpdateOrder(order);
 
-//Console.WriteLine("Detaching SMS Observer...");
-//_orderService.Detach(smsObs);
+Console.WriteLine("Detaching SMS Observer...");
+_orderService.Detach(smsObs);
 
-//Console.WriteLine("Updating Order Status...");
-//_orderService.UpdateOrder(order);
+Console.WriteLine("Updating Order Status...");
+_orderService.UpdateOrder(order);
 
 #endregion
 
